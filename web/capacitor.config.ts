125d4bd47270cafab5fd18934b1daacf4f98d737
hanpiw <alhafidz.almagribi409@gmail.com>
2026-07-08 17:44:51 +0700

    Update

---
+++ web/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hanpiw.fishfeeder',
+  appName: 'Ngefeed Fishhh!',
+  webDir: 'out'
+};
+
+export default config;
